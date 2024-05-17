c3fec5c50bcac5d4b04b888dc68898c078707365
dbaranov <dbaranov@findmykids.org>
2024-05-17 22:13:52 +0300

    removed svg to Text

--- app/src/main/kotlin/com/mugss/mugss/app/main/LauncherActivity.kt
+++ app/src/main/kotlin/com/mugss/mugss/app/main/LauncherActivity.kt
@@ -2,22 +2,24 @@ package com.mugss.mugss.app.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.mugss.core.compose.theme.MuGssTheme
+import com.mugss.core.compose.theme.typo.shadow.ShadowOffset
+import com.mugss.core.compose.theme.typo.shadow.withShadow
 import com.mugss.mugss.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LauncherActivity : ComponentActivity() {
 
@@ -28,14 +30,27 @@ class LauncherActivity : ComponentActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContent {
             MuGssTheme {
-                Box(modifier = Modifier.fillMaxSize().background(MuGssTheme.colors.white), contentAlignment = Alignment.Center) {
-                    Image(painter = painterResource(R.drawable.ic_splash), contentDescription = null)
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MuGssTheme.colors.white), contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = getString(R.string.app_name),
+                        style = MuGssTheme.typography.titleXXl.withShadow(ShadowOffset.BIG),
+                        color = MuGssTheme.colors.primary
+                    )
                 }
             }
         }
-        Handler(Looper.getMainLooper()).postDelayed({
+        lifecycleScope.launch {
+            delay(LAUNCHER_SHOWING_DELAY)
             startActivity(Intent(this@LauncherActivity, MainActivity::class.java))
             finish()
-        }, 3000L)
+        }
+    }
+
+    companion object {
+        private const val LAUNCHER_SHOWING_DELAY = 3000L
     }
 }
